6f85e06fde3eb1e1d05b47f74d1572b6fbe911e1
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2024-03-27 23:22:11 +0600

    Added Books section with manual data

--- src/Components/Root/Home/Books/Books.jsx
+++ src/Components/Root/Home/Books/Books.jsx
@@ -1,8 +1,14 @@
-
+import Card from "./Card/Card";
 
 const Books = () => {
   return (
-    <div >
+    <div className="mt-10 lg:mt-16">
+       <h1 className="text-4xl play font-bold text-center my-10">Books</h1>
+       <div className="grid lg:grid-cols-3 gap-4">
+       <Card/>
+       <Card/>
+       <Card/>
+       </div>
       
     </div>
   );
--- src/Components/Root/Home/Books/Card/Card.jsx
+++ src/Components/Root/Home/Books/Card/Card.jsx
@@ -1,8 +1,32 @@
-
+import { IoIosStarOutline } from "react-icons/io";
 
 const Card = () => {
   return (
-    <div>
+    <div className="card border-2 w-full p-10 shadow-xl">
+
+      {/* Card image */}
+      <div>
+        <figure className="bg-base-200 rounded-2xl p-10">
+          <img
+            src="https://i.ibb.co/7tbZzc1/Journey-of-Prophet-Muhammad.jpg"
+            className="w-2/3  skew-y-6 rounded-lg shadow-2xl"
+          />
+        </figure>
+      </div>
+
+     {/* Cards Info */}
+      <div className="mt-6">
+
+        <h2 className="lg:text-2xl text-xl font-bold play mb-3">The Journey of Prophet Muhammad</h2>
+        <p>By: Muhamad Tanvir Hasan</p>
+
+        <hr className="border-dashed w-[90%] mx-auto my-3" />
+
+        <div className="flex justify-between">
+          <p>Fiction</p>
+          <p className="flex items-center gap-2">4.5 <IoIosStarOutline /></p>
+        </div>
+      </div>
     </div>
   );
 };
