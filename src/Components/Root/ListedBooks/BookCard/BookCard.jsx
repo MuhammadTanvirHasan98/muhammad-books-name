d9ab33093c47f165f853eea094f5b6021b0fb388
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-03-29 23:48:55 +0600

    finished sorting

---
+++ src/Components/Root/ListedBooks/BookCard/BookCard.jsx
@@ -0,0 +1,102 @@
+import { IoLocationOutline, IoPeople } from "react-icons/io5";
+import { BiBookOpen } from "react-icons/bi";
+import PropTypes from "prop-types";
+
+const BookCard = ({ book }) => {
+  console.log(book);
+  const {
+    bookName,
+    author,
+    image,
+    publisher,
+    category,
+    yearOfPublishing,
+    totalPages,
+    tags,
+    rating,
+  } = book;
+
+  return (
+    <div className="p-5 lg:p-8 my-8 border-2 rounded-2xl">
+      <div className="flex lg:flex-row flex-col gap-6">
+        {/* Image div */}
+        <div className="flex items-center">
+          <figure className="bg-base-200 rounded-2xl w-full flex justify-center py-10">
+            <img
+              src={image}
+              className="w-[45%]  skew-y-6 rounded-lg shadow-2xl"
+            />
+          </figure>
+        </div>
+
+        <div className="lg:w-3/4 w-full">
+          <h1 className="text-2xl lg:text-3xl lg:mb-4  font-bold play">
+            {bookName}
+          </h1>
+          <p>
+            By: <span className=" font-semibold">{author}</span>
+          </p>
+
+          {/* Tags and year div*/}
+          <div className="flex lg:flex-row flex-col lg:gap-10">
+            <div className="flex flex-wrap items-center gap-4  font-semibold lg:my-4">
+              <p>Tags:</p>
+
+              <p className="bg-green-50 p-2 rounded-xl text-green-600">
+                #{tags[0]}
+              </p>
+              <p className="bg-green-50 p-2 rounded-xl text-green-600">
+                #{tags[1]}
+              </p>
+            </div>
+            <div className="flex gap-2 items-center">
+              <IoLocationOutline />
+              <p>Year of Publishing:</p>
+              <p>{yearOfPublishing}</p>
+            </div>
+          </div>
+
+          {/* Pages and Publisher div */}
+          <div className="flex flex-col lg:flex-row  lg:gap-10">
+            <p className="flex items-center gap-2">
+              <IoPeople />
+              Publisher: {publisher}
+            </p>
+            <p className="flex items-center gap-2">
+              <BiBookOpen />
+              Pages:{totalPages}
+            </p>
+          </div>
+
+          <hr className="border-dashed border-green-600 my-3" />
+
+          {/* Footer and button section */}
+          <div>
+            <div className="flex flex-col lg:flex-row gap-4 mt-4">
+              <p className="p-3 bg-sky-200   text-blue-600 rounded-3xl">
+                Category: {category}
+              </p>
+              <p className="p-3 bg-sky-200  text-blue-600 rounded-3xl">
+                Rating: {rating}
+              </p>
+
+              <button
+                className="btn 
+            rounded-3xl
+            lg:text-xl border-2 border-blue-500 bg-gradient-to-r from-blue-400 to-green-500 text-white"
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BookCard.propTypes = {
+  book: PropTypes.object,
+};
+
+export default BookCard;
--- src/Components/Root/ListedBooks/ListedBooks.jsx
+++ src/Components/Root/ListedBooks/ListedBooks.jsx
@@ -1,57 +1,100 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import ReadBooks from './ReadBooks/ReadBooks';
-import WishlistBooks from './WishlistBooks/WishlistBooks';
-import { useLoaderData } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { getStoredBooks } from '../../../Utility/localStorage';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import BookCard from "./BookCard/BookCard";
+import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getStoredBooks } from "../../../Utility/localStorage";
 
 const ListedBooks = () => {
-    const [readBooks, setReadBooks] = useState([]);
-    const [wishlistBooks, setWishlistBooks] = useState([]);
+  const [readBooks, setReadBooks] = useState([]);
+  const [wishlistBooks, setWishlistBooks] = useState([]);
 
-     const markedBooks = useLoaderData();
-     
-     console.log(markedBooks);
+  const markedBooks = useLoaderData();
 
-     useEffect(()=>{
-        const storedReadBooksId = getStoredBooks('read');
-        const storedWishlistBooksId = getStoredBooks('wish-list');
+  console.log(markedBooks);
 
-        if(markedBooks.length>0){
-           const booksRead = markedBooks.filter(book => storedReadBooksId.includes(book.bookId));
-           setReadBooks(booksRead);
+  useEffect(() => {
+    const storedReadBooksId = getStoredBooks("read");
+    const storedWishlistBooksId = getStoredBooks("wish-list");
 
-            const booksWishlist = markedBooks.filter(book => storedWishlistBooksId.includes(book.bookId));
-            setWishlistBooks(booksWishlist);
-        }
-     },[])
+    if (markedBooks.length > 0) {
+      const booksRead = markedBooks.filter((book) =>
+        storedReadBooksId.includes(book.bookId)
+      );
+      setReadBooks(booksRead);
+
+      const booksWishlist = markedBooks.filter((book) =>
+        storedWishlistBooksId.includes(book.bookId)
+      );
+      setWishlistBooks(booksWishlist);
+    }
+  }, [markedBooks]);
+
+  const handleSort = (type) => {
+    if (type === "rating") {
+      setReadBooks((prevBooks) => {
+        prevBooks.sort((a, b) => b.rating - a.rating);
+        return [...prevBooks];
+      });
+      setWishlistBooks((prevBooks) => {
+        prevBooks.sort((a, b) => b.rating - a.rating);
+        return [...prevBooks];
+      });
+    } else if (type === "num-of-pages") {
+      setReadBooks((prevBooks) => {
+        prevBooks.sort((a, b) => b.totalPages - a.totalPages);
+        return [...prevBooks];
+      });
+      setWishlistBooks((prevBooks) => {
+        prevBooks.sort((a, b) => b.totalPages - a.totalPages);
+        return [...prevBooks];
+      });
+    } else if (type === "published-year") {
+      setReadBooks((prevBooks) => {
+        prevBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        return [...prevBooks];
+      });
+      setWishlistBooks((prevBooks) => {
+        prevBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        return [...prevBooks];
+      });
+    }
+  };
 
   return (
-    <div>
-      <h1 className='text-center text-4xl my-10'>Tabs are here</h1>
-    <Tabs>
-    <TabList>
-      <Tab>Read Books</Tab>
-      <Tab>Wishlist Books</Tab>
-    </TabList>
+    <div className="my-20">
+      <div className="text-center">
+        <details className="dropdown">
+          <summary className="m-1 btn btn-lg bg-gradient-to-r from-blue-400 to-green-500 text-white">Sort By</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box space-y-1 w-52">
+            <li onClick={() => handleSort('rating')} className="bg-gradient-to-r from-blue-400 to-green-500 text-white py-1 cursor-pointer rounded-box">Rating</li>
+            <li onClick={() => handleSort('num-of-pages')} className="bg-gradient-to-r from-blue-400 to-green-500 text-white py-1 cursor-pointer rounded-box">Number of Pages</li>
+            <li onClick={() => handleSort('published-year')} className="bg-gradient-to-r from-blue-400 to-green-500 text-white py-1 cursor-pointer rounded-box">Published Year</li>
+          </ul>
+        </details>
+      </div>
 
-    <TabPanel>
-      <div className='my-10'>
-        {
-          readBooks.map(book => <ReadBooks key={book.bookId} book={book}/>)
-        }
-      </div>
-     
-    </TabPanel>
-    <TabPanel>
-      <div className='my-10'>
-        {
-          wishlistBooks.map(book => <WishlistBooks key={book.bookId} book={book}/>)
-        }
-      </div>
-    </TabPanel>
-  </Tabs>
+      <Tabs>
+        <TabList>
+          <Tab>Read Books</Tab>
+          <Tab>Wishlist Books</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="my-10">
+            {readBooks.map((book) => (
+              <BookCard key={book.bookId} book={book} />
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="my-10">
+            {wishlistBooks.map((book) => (
+              <BookCard key={book.bookId} book={book} />
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
